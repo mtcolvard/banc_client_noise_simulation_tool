0798907638dc6c9cbc651eac0b0f9fb3d4aa4145
Matty Talty <matthew.colvard@gmail.com>
2023-05-05 16:28:02 -0700

    escaped appostaphes

--- app/page.js
+++ app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Lee Brenner's Noise Abatement Simulator
+          Lee Brenners Noise Abatement Simulator
         </p>
         {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -67,7 +67,7 @@ export default function Home() {
           Different levels of noise reduction in a soundproofing remodeling project, involve radically different solutions and costs. 
         </p>
         <p className={`mb-6 text-xl opacity-50`}>
-          How can you know you will be getting your money's worth to achieve the noise reduction you need?
+          How can you know you will be getting your moneys worth to achieve the noise reduction you need?
         </p>
         <p className={`mb-8 text-xl opacity-50`}>
           With a pair of headphones and this algorithm, we can assist you in estimating the amount of intervention you require.
