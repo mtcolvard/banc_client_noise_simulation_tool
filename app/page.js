a6b1093f6fce4ce8bcf5417fd40b11f8675c0a1c
Matty Talty <matthew.colvard@gmail.com>
2023-05-08 09:40:32 -0700

    tabs and grids are displaying with proper styling for current selection

--- app/page.js
+++ app/page.js
@@ -84,10 +84,10 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-3 py-28 md:p-24 xl:px-56">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono md:text-3xl lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit ">
-          Lee Brenners Noise Abatement Simulator
+    <main className="flex min-h-screen flex-col items-center justify-between px-3 pb-28 md:px-24 md:pb-24 xl:px-56">
+      <div className="z-10 md:-pt-24 w-full  items-center justify-between font-mono md:text-3xl lg:flex">
+        <p className=" left-0 top-0 flex w-full text-center justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit ">
+            Lee Brenner&rsquo;s Noise Abatement Simulator
         </p>
       </div>
       <div className="md:mt-12 mb-32 grid  lg:mb-0  lg:text-left font-mono">
@@ -95,21 +95,21 @@ export default function Home() {
           Different levels of noise reduction in a soundproofing remodeling project, involve radically different solutions and costs. 
         </p>
         <p className={`mb-6 text-xl opacity-50`}>
-          How can you know you will be getting your moneys worth to achieve the noise reduction you need?
+          How can you know you will be getting your money&rsquo;s worth to achieve the noise reduction you need?
         </p>
         <p className={`mb-8 text-xl opacity-50`}>
           With a pair of headphones and this algorithm, we can assist you in estimating the amount of intervention you require.
         </p>
-        <p className={`mb-3 text-2xl font-semibold`}>
+        <p className={`mb-4 text-2xl font-semibold`}>
           Instructions
         </p>
-        <p className={`mb-3 text-xl opacity-50`}>
+        <p className={`mb-4 text-xl opacity-50`}>
           1) Go to the location where you feel most oppressed by the noise and make a mental note of its loudness.
         </p>
-        <p className={`mb-3 text-xl opacity-50`}>
+        <p className={`mb-4 text-xl opacity-50`}>
           2) In a more relaxing location on your property, plug a pair of headphones into your computer or phone.
         </p>
-        <p className={`mb-3 text-xl opacity-50`}>
+        <p className={`mb-1 text-xl opacity-50`}>
           3) Select a noise source:
         </p>
 
@@ -117,7 +117,7 @@ export default function Home() {
           sendNoiseSource={(noiseSource) => setSelectedSound(noiseSource)}
         />
         <div className={`m-4`}></div>
-        <p className={`mb-3 text-xl opacity-50`}>
+        <p className={`mb-1 text-xl opacity-50`}>
           4) Hit play.
         </p>
 
@@ -126,10 +126,10 @@ export default function Home() {
           sendPlayControlClick={(playControl) => handlePlayControlClick(playControl) }
         />
         <div className={`m-4`}></div>
-        <p className={`mb-3 text-xl opacity-50`}>
+        <p className={`mb-4 text-xl opacity-50`}>
           5) Using the volume controls on your device, adjust the volume until you feel the sound recording approximates the same level as the noise source that has been disturbing you.
         </p>
-        <p className={`mb-3 text-xl opacity-50`}>
+        <p className={`mb-1 text-xl opacity-50`}>
           6) Select a level of noise reduction and determine which one works to meet your needs:
         </p>
 
@@ -145,7 +145,7 @@ export default function Home() {
           // isDisabled={isDisabled}
         />
         <div className={`m-4`}></div>
-        <p className={`mb-3 text-xl opacity-50`}>
+        <p className={`mb-4 text-xl opacity-50`}>
           Once you have determined the level of noise reduction you need, it is possible to price out the architecural acoustics plan to get you there.
         </p>
 
--- components/noiseSourceTabs.js
+++ components/noiseSourceTabs.js
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import { TruckIcon, ArrowDownTrayIcon, UsersIcon, WrenchIcon} from '@heroicons/react/20/solid'
-
+import {TruckIcon, ArrowDownTrayIcon, UsersIcon, WrenchIcon, MusicalNoteIcon, TvIcon, TrashIcon, SignalIcon} from '@heroicons/react/20/solid'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -9,12 +8,15 @@ function classNames(...classes) {
 export default function NoiseSourceTabs({ sendNoiseSource }) {
 
   const [noiseSource, setNoiseSource] = useState([
-    { name: 'Loud Neighbors', value: 'loudNeighbors', icon: UsersIcon, current: true },
+    { name: 'Neighbors Arguing', value: 'loudNeighbors', icon: UsersIcon, current: true },
     { name: 'Noisy Traffic', value: 'noisyTraffic', icon: TruckIcon, current: false },
+    { name: 'Loud Television', value: 'loudTelevision', icon: TvIcon, current: false },
+    { name: 'Mid Frequency Music', value: 'midFrequencyMusic', icon: MusicalNoteIcon, current: false },
+    { name: 'Upstairs Stomping', value: 'upstairsStomping', icon: ArrowDownTrayIcon, current: false },
+    { name: 'Garbage Collection', value: 'garbageCollection', icon: TrashIcon , current: false },
     { name: 'Mechanical Noise', value: 'mechanicalNoise', icon: WrenchIcon, current: false },
-    { name: 'Annoying Footfalls', value: 'annoyingFootfalls', icon: ArrowDownTrayIcon, current: false },
+    { name: 'Low Freqency Music', value: 'lowFrequencyMusic', icon: SignalIcon, current: false },
   ])
-
 
   function handleClick(tabName) {
     setNoiseSource(noiseSource.map(item => {
@@ -30,10 +32,11 @@ export default function NoiseSourceTabs({ sendNoiseSource }) {
 
   return (
     <div>
-      <div className="">
+      <div>
       {/* <div className="hidden sm:block"> */}
         <div className="border-b border-gray-200">
-          <div className="-mb-px  md:space-x-8 flex flex-col md:inline divide-y md:divide-y-0 divide-white md:-gray-200" aria-label="Tabs">
+          <container className="-mb-px   flex flex-col md:grid md:grid-cols-4 md:grid-rows-2" aria-label="Tabs">
+          {/* <container className="-mb-px   flex flex-col md:grid md:grid-cols-4 md:grid-rows-2  max-sm:divide-y  max-sm:divide-white" aria-label="Tabs"> */}
             {noiseSource.map((tab) => (
               <button
                 key={tab.name}
@@ -41,8 +44,8 @@ export default function NoiseSourceTabs({ sendNoiseSource }) {
                 className={classNames(
                   tab.current
                     ? 'border-indigo-500 text-indigo-600'
-                    : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                  ' group inline-flex  items-center  border-y-2 md:border-y-0 md:border-b-2 py-4 px-1 text-sm font-medium'
+                    : ' text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                  ' group inline-flex  items-center  border-b-2  py-4 px-1 text-sm font-medium'
                 )}
                 aria-current={tab.current ? 'page' : undefined}
               >
@@ -56,7 +59,7 @@ export default function NoiseSourceTabs({ sendNoiseSource }) {
                 <span>{tab.name}</span>
               </button>
             ))}
-          </div>
+          </container>
         </div>
       </div>
     </div>
